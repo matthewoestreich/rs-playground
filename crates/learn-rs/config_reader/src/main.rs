ff9c7984540c149f57dab2903ad797e7454a33d5
Matt Oestreich <matthewpoestreich@gmail.com>
2025-11-12 13:28:13 -0600

    update

--- crates/learn-rs/config_reader/src/main.rs
+++ crates/learn-rs/config_reader/src/main.rs
@@ -5,13 +5,13 @@ use serde::{Deserialize, Serialize};
 fn main() {
     let mut args = std::env::args();
     // Unwrapping is OK here, as UTF-8 Strings can always be converted to PathBufs
-    let Some(path) = args.nth(1).map(|a| PathBuf::try_from(a).unwrap()) else {
+    let Some(path) = args.nth(1).map(PathBuf::from) else {
         eprintln!("Please specify the input path");
         return;
     };
     // Unwrapping is Ok as `path` was created from UTF-8 string, and so is the extension
     let _extension = path.extension().map(|o| o.to_str().unwrap());
-    let file_contents = match std::fs::read_to_string(&path) {
+    let _file_contents = match std::fs::read_to_string(&path) {
         Ok(c) => c,
         Err(e) => {
             // `path` was created from an UTF-8 string, so can be converted to one
@@ -24,10 +24,8 @@ fn main() {
         }
     };
 
-    let config: Config = todo!(
-        "Deserialize `file_contents` using either serde_yaml or serde_json depending on the file extension. Use dynamic dispatch"
-    );
-
+    #[allow(clippy::diverging_sub_expression)]
+    let config: Config = todo!("Deserialize `file_contents` using either serde_yaml or serde_json depending on the file extension. Use dynamic dispatch");
     println!("Config was: {config:?}");
 }
 
